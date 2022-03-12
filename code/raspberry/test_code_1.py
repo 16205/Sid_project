2b3ee6aaabe9012c746b8534a74baa3426b406b1
RaphJad <21295@ecam.be>
2022-03-12 09:25:50 +0100

    Clean the stereovision folder

--- code/raspberry/test_code_1.py
+++
@@ -1,8 +0,0 @@
-import os
-import time
-scan_name = "test_code"
-name_picture_r = "nikmouk"
-print(type(scan_name))
-print(type(name_picture_r))
-os.system('python3 prise_image_bon.py '+scan_name+' '+name_picture_r)
-time.sleep(10)
