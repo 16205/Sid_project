8bb839097c8a18f72f77f53e65301e78902ea67e
wasanico <salocin.sam@gmail.com>
2022-03-26 16:28:02 +0100

    shoots pics

--- code/raspberry/scan.py
+++ code/raspberry/scan.py
@@ -1,18 +1,18 @@
 #script to take the images for a scan
 #arg1 : the scan's name
 #arg2 : the number of steps
-from tkinter import N
 import cv2
 import os
 import pexpect
 from pexpect import pxssh
 import sys
-#import sshTools
+import sshTools
 import RPi.GPIO as GPIO
 from RpiMotorLib import RpiMotorLib
-# import stepper
-# import laser
 import time
+import jsonTools as jt
+import red_pixs as rp
+from msilib.schema import Directory
 from raspberry.stepper import *
 from raspberry.laser import *
 # reading the scan_name from the input
@@ -29,6 +29,11 @@ def runScan(quality, laser_power = 30, color_space="Red", scale=10, scan_name = 
     - scale = size of the piece to scan (5,10,15,20)\n
     - scan_name = name of the scan (if None => 'test')
     """
+
+    # on crée les dossier pour le scan dans le master et dans la slave 
+    os.system('mkdir '+ scan_name) #on crée le dossier dans le master
+    sshTools.createFolderSlave(scan_name) #on crée le dossier dans le slave
+    print("folders created")
     
     print(scan_name)
     print(quality)
@@ -58,14 +63,26 @@ def runScan(quality, laser_power = 30, color_space="Red", scale=10, scan_name = 
     setDuty(laser_power)
     turnLaserOn()
 
-    
-    for i in range(step_nbr):
+    for i in range(1, step_nbr):
 
+        name_picture_r = scan_name +"_R_"+str(i) # create the file name for the i th picture from the rigth camera
+        name_picture_l = scan_name+"_L_"+str(i)  # create the filename for the i th picture from the left camera
+        # on prend les images
+        print("name_picture_r = ", name_picture_r)
+        print("name_picture_l = ", name_picture_l)
+
+        os.system('python3 prise_image_bon.py '+scan_name+' '+name_picture_r) #on pose que le master sera la camera de droite
+        print("im master taken")
+        sshTools.takePictureWithSlave(scan_name, name_picture_l)
+        print("im slave taken")
 
         # Run the stepper
         makeStep(step_number,step_size, doEnd=True)
-        time.sleep(.2)
-
+        time.sleep(2)
+        #on recup l'image qu'on vient de prendre
+        sshTools.getPictureSlave(scan_name, name_picture_l)
+        time.sleep(2)
+        
     # turn laser off
     turnLaserOff()
 
@@ -75,3 +92,46 @@ def runScan(quality, laser_power = 30, color_space="Red", scale=10, scan_name = 
     GPIO.cleanup() # clear GPIO allocations after run
     
 #runScan("test")
+
+def scan2json(scan_name):
+    jt.build_Json4scan(scan_name)
+    files = os.listdir(scan_name)
+    json_name = scan_name + '.json'
+    for i in range(len(files)/2):
+        if i != 113:
+            #on intègre l'imagede gauche dans le json 
+            picture_name = scan_name+'_L_'+str(i)+'.jpg'
+            pict_path = scan_name+'/'+picture_name
+            img = cv2.imread(pict_path)
+            coords = rp.get_red_points_coordinates(img,'saturation')
+            jt.addNpArray2Json(coords, json_name, picture_name)
+            #on intègre l'image de droite dans le json 
+            picture_name = scan_name+'_R_'+str(i)+'.jpg'
+            pict_path = scan_name+'/'+picture_name
+            img = cv2.imread(pict_path)
+            coords = rp.get_red_points_coordinates(img,'saturation')
+            jt.addNpArray2Json(coords, json_name, picture_name)
+
+
+def scan2jsonFinal(scan_name, color):
+        jt.build_Json4scan(scan_name)
+        files = os.listdir(scan_name)
+        json_name = scan_name + '.json'
+        dico = {}
+        for i in range(int(len(files)/2)):
+            if i != 113:
+                #on intègre l'imagede gauche dans le json 
+                picture_name = scan_name+'_L_'+str(i)+'.jpg'
+                pict2Dico(scan_name, picture_name, color, dico)
+                #on intègre l'image de droite dans le json
+                picture_name = scan_name+'_R_'+str(i)+'.jpg'
+                pict2Dico(scan_name, picture_name, color, dico)
+                print(i)
+        jt.Dico2Array(dico, json_name)
+
+def pict2Dico(scan_name, picture_name, color, dico):
+    pict_path = scan_name+'/'+picture_name
+    img = cv2.imread(pict_path)
+    coords = rp.get_avg_red_pixs(img,color)
+    list = coords.tolist()
+    dico.update({picture_name:list})
