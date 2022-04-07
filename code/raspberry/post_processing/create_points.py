52382c861a5086f748756423ac2f7c2a2476ffcd
wasanico <salocin.sam@gmail.com>
2022-04-07 21:22:02 +0200

    mesh can be created

--- code/raspberry/post_processing/create_points.py
+++ code/raspberry/post_processing/create_points.py
@@ -3,20 +3,74 @@ import numpy as np
 import random
 from mpl_toolkits.mplot3d import axes3d
 import json
+import math
+
+def create_sphere():
+    x = []
+    y = []
+    z = []
+    for i in range(2000):
+        u = np.random.normal(0,1)
+        v = np.random.normal(0,1)
+        w = np.random.normal(0,1)
+        norm = (u*u + v*v + w*w)**(0.5)
+        xi,yi,zi = u/norm,v/norm,w/norm
+        x.append(xi)
+        y.append(yi)
+        z.append(zi)
+
+    
+    coords = []
+    for i in range(len(x)):
+        coords.append([x[i],y[i],z[i]])
+    
+    # create file
+    with open('code/raspberry/post_processing/json_data.json', 'w') as outfile:
+        json.dump(coords, outfile)
 
 
-x = []
-y = []
-z = []
-for i in range(2000):
-    u = np.random.normal(0,1)
-    v = np.random.normal(0,1)
-    w = np.random.normal(0,1)
-    norm = (u*u + v*v + w*w)**(0.5)
-    xi,yi,zi = u/norm,v/norm,w/norm
-    x.append(xi)
-    y.append(yi)
-    z.append(zi)
+
+def create_cube():
+    x = [0,0,2,2,2,2,0,0]
+    y = [0,0,0,0,2,2,2,2]
+    z = [0,2,0,2,0,2,0,2]
+
+    coords = []
+    for i in range(len(x)):
+        coords.append([x[i],y[i],z[i]])
+    
+    # create file
+    with open('code/raspberry/post_processing/json_cube.json', 'w') as outfile:
+        json.dump(coords, outfile)
+
+    return x,y,z
+
+# x,y,z = create_cube()
+
+def create_cylinder(columns=3):
+    x = []
+    y = []
+    z = []
+
+    angle = 360/columns
+
+    for i in range(columns):
+        alpha = math.radians(angle * i)
+        for j in range(3):
+        
+            x.append(math.cos(alpha))
+            y.append(math.sin(alpha))
+            z.append(j)
+    
+    coords = []
+    for i in range(len(x)):
+        coords.append([x[i],y[i],z[i]])
+
+    with open('code/raspberry/post_processing/cylinder.json', 'w') as outfile:
+        json.dump(coords, outfile)
+
+    return x,y,z
+x,y,z = create_cylinder(12)
 
 # plot
 
@@ -25,12 +79,3 @@ for i in range(2000):
 # ax.scatter(x, y, z, s=100, c='r', zorder=10)
 # ax.set_title('Example of a uniformly sampled sphere', fontdict={'fontsize':20})
 # plt.show()
-
-# create file
-
-coords = []
-for i in range(len(x)):
-    coords.append([x[i],y[i],z[i]])
-
-with open('code/raspberry/post_processing/json_data.json', 'w') as outfile:
-    json.dump(coords, outfile)
--- code/raspberry/post_processing/create_stl.py
+++ code/raspberry/post_processing/create_stl.py
@@ -1,69 +1,75 @@
-from pickletools import int4
 from stl import mesh
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
 import json
 
 """
 https://stackoverflow.com/questions/60066405/create-a-stl-file-from-a-collection-of-points
 https://www.google.com/search?q=create+stl+file+from+3D+points&client=firefox-b-d&sxsrf=APq-WBtNJQR-aHtoFrPQ1u-YXHlYW6KtoA%3A1648658438709&ei=BohEYtfhKpr-sAfSo7vYCA&ved=0ahUKEwjX_OCko-72AhUaP-wKHdLRDosQ4dUDCA0&uact=5&oq=create+stl+file+from+3D+points&gs_lcp=Cgdnd3Mtd2l6EAM6BwgAEEcQsANKBAhBGABKBAhGGABQ0w1YlBhg6hpoAnABeACAAVKIAakCkgEBNJgBAKABAcgBCMABAQ&sclient=gws-wiz
 
-other idea:
-        - create XYZ file from 3D coordinates
-        - then convert to stl file
 
-STEPS : v get xyz coordinates
-        v put it in an array of points with dtype mesh
-        - /!\ COORDINATES MUST BE POSITIVE
-        - create a mesh
-        - generate normals
-        - create triangles
-        - generate stl file
-        - voila
+STEPS : 
+    - import xyz points
+    - set number of columns
+    - create triangles 
+        - side : 2 triangles for 2 lines and 2 columns
+        - top & bottom : (number of columns - 2) triangles
+    - create mesh
+    - tadaaaaaaaa
 """
 
-with open('code/raspberry/post_processing/json_data.json') as json_file:
-    data = json.load(json_file)
+def create_stl(columns):
+    # create vertices (or points)
+    with open('code/raspberry/post_processing/cylinder.json') as json_file:
+        data = json.load(json_file)
+    vertices = np.array(data)
+
+    # number of points per collumn
+    lines = (int)(len(data)/columns)
+    
+    # create triangles
+    triangles = []
+    for i in range(columns):
+        k = 0
+        # if last column, connect to the first one
+        if i == columns -1: 
+            k = len(data)
+        # triangles must be constructed clock-wise from an outside point of view
+        for j in range(lines-1):
+            # first triangle ex [0,3,1] (if 3 lines)
+            triangles.append([
+                i * lines + j, 
+                i * lines + j + lines - k, 
+                i * lines + j + 1])
+            # second triange ex [1,3,4]
+            triangles.append([
+                i * lines + j + 1, 
+                i * lines + j + lines - k, 
+                i * lines + j + lines + 1 - k])
+
+    # top and bottom triangles
+    for i in range(columns-2):
+        top = lines - 1
+        # top triangle ex [2,5,8] (if 3 lines)
+        triangles.append([top, i * lines + top + lines, i * lines + top + 2 * lines])
+        # bottom triangle ex [0,6,3]
+        triangles.append([0, i * lines + 2 * lines, i * lines + lines])
+
+    # transform to numpy array
+    faces = np.array(triangles)
+    
+    return vertices, faces
 
 
-points = np.array(data, dtype=mesh.Mesh.dtype)
 
-#TODO: faire en sorte que tous les points soient positifs (1er quadrant)
+def create_mesh(vertices, faces):
+    new_stl = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+    for i, f in enumerate(faces):
+        for j in range(3):
+            new_stl.vectors[i][j] = vertices[f[j],:]
 
-# test = np.zeros(100, dtype=mesh.Mesh.dtype)
-# print(points)
-your_mesh = mesh.Mesh(points, remove_empty_areas=False)
+    # Write the mesh to file "new_stl.stl"
+    new_stl.save('code/raspberry/post_processing/new_stl.stl')
 
-# The mesh normals (calculated automatically)
-your_mesh.normals
-# The mesh vectors
-your_mesh.v0, your_mesh.v1, your_mesh.v2
-# Accessing individual points (concatenation of v0, v1 and v2 in triplets)
-
-# print(your_mesh.points[0][0:3])
-# assert (your_mesh.points[0][0:3] == your_mesh.v0[0]).all()
-# assert (your_mesh.points[0][3:6] == your_mesh.v1[0]).all()
-# assert (your_mesh.points[0][6:9] == your_mesh.v2[0]).all()
-# assert (your_mesh.points[1][0:3] == your_mesh.v0[1]).all()
-
-
-your_mesh.save('code/raspberry/post_processing/new_stl_file.stl')
-
-
-# from mpl_toolkits import mplot3d
-# from matplotlib import pyplot
-
-# # Create a new plot
-# figure = pyplot.figure()
-# axes = mplot3d.Axes3D(figure)
-
-# # Load the STL files and add the vectors to the plot
-# your_mesh = mesh.Mesh.from_file('code/raspberry/post_processing/new_stl_file.stl')
-# axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
-
-# # Auto scale to the mesh size
-# scale = your_mesh.points.flatten()
-# axes.auto_scale_xyz(scale, scale, scale)
-
-# # Show the plot to the screen
-# pyplot.show()
+# do
+vertices, faces = create_stl(12)
+create_mesh(vertices, faces)
