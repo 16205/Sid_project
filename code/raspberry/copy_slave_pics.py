fe0fe988536dab0d8a2b72106c5c0549716c160d
RaphJad <21295@ecam.be>
2022-05-08 18:47:24 +0200

    uncomment

--- code/raspberry/copy_slave_pics.py
+++ code/raspberry/copy_slave_pics.py
@@ -9,15 +9,3 @@ def copy_slave_pics(scan_folder):
 
     # copy folder name
     shutil.copytree(source_folder, dest_folder)
-
-    # # copy files in the folder
-    # for file_name in os.listdir(source_folder):
-    #     source = source_folder + file_name
-    #     dest = dest_folder + file_name
-        
-    #     try:
-    #         if os.path.isfile(source):
-    #             shutil.copy(source, dest)
-    #             print('copied', file_name)
-    #     except os.error as e:
-    #         print(str(e))
