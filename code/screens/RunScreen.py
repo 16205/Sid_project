3b82b8297e7206a8ea84b39abbe2aeaa32282c4d
Nicolas Samelson <salocin.sam@gmail.com>
2022-05-10 23:37:10 +0200

    non

--- code/screens/RunScreen.py
+++ code/screens/RunScreen.py
@@ -53,10 +53,10 @@ class RunScreen(Screen):
 
         buf1 = cv.flip(color, 0)
         buf = buf1.tostring()
-        self.image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='luminance')
-        self.image_texture.blit_buffer(buf, colorfmt='luminance', bufferfmt='ubyte')
+        image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='luminance')
+        image_texture.blit_buffer(buf, colorfmt='luminance', bufferfmt='ubyte')
         # display image from the texture
-        self.img1.texture = self.image_texture
+        self.img1.texture = image_texture
     
 
     def addWidgets(self):
@@ -215,10 +215,8 @@ class RunScreen(Screen):
         content = GridLayout(cols=1)
 
         # capture 
-        try:
-            scanImg = self.image_texture
-        except:
-            scanImg = Image(source='res/sid.jpg')
+        scanImg = Image(source='res/sid.jpg')
+
         # progress bar
         
         pb = ProgressBar(value=0, max=100,size_hint_y=None, height=self.winSize[1]/15)
