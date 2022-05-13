e06ef28b657e64cc3533fd14851b7c4999a60ffe
16205 <78419842+16205@users.noreply.github.com>
2022-05-13 10:36:02 +0200

    Delete code directory

--- code/raspberry/stream.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env python
-
-import os
-# os.system('raspivid -t 0 -hf -vf -w 1296 -h 972 --nopreview -o - | nc -l 5000')
-os.system("raspivid -o - -t 0 -w 1296 -h 972 -fps 12  | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8080/}' :demux=h264")
